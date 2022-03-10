bbadd7d87c7a1a399a4e8fb6157ee5b558ef8263
NathanFelipe99 <nathanfguerlando@gmail.com>
2022-03-10 08:58:08 -0300

    atualização do script

---
+++ regulacaomedica.js
@@ -0,0 +1,7 @@
+{
+    "anEndLogradouro": "frmshc.CadLocal.Main.Cadastro.anEndLogradouro",
+    "anEndBairro": "frmshc.CadLocal.Main.Cadastro.anEndBairro",
+    "anEndCidade": "frmshc.CadLocal.Main.Cadastro.anEndCidade",
+    "anEndUF": "frmshc.CadLocal.Main.Cadastro.anEndUF"
+}
+

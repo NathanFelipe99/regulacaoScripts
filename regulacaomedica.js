74b307ae944c33ddcfa8da3d64bf20c47b953812
NathanFelipe99 <nathanfguerlando@gmail.com>
2022-04-18 13:18:11 -0300

    excluindo trash

--- regulacaomedica.js
+++
@@ -1,7 +0,0 @@
-{
-    "anEndLogradouro": "frmshc.CadLocal.Main.Cadastro.anEndLogradouro",
-    "anEndBairro": "frmshc.CadLocal.Main.Cadastro.anEndBairro",
-    "anEndCidade": "frmshc.CadLocal.Main.Cadastro.anEndCidade",
-    "anEndUF": "frmshc.CadLocal.Main.Cadastro.anEndUF"
-}
-
--- seila.js
+++
@@ -1,5 +0,0 @@
-wHtml += `<h4 style="background: lightgrey;margin-top: 5px;padding: 5px;border-radius: 5px;" name="anPergunta" for="${
-  wScriptItem.cnRegulacaoScript
-}-${wScriptItem.csRegulacaoScriptItem}">${wScriptItem.anInteracaoTexto} ${
-  wScriptItem.boRequerido ? '<sup class="text-danger">*</sup>' : ""
-}</h4>`;
